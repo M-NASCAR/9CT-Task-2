bd7cbed13aeaae6381e7eee0b35636e49290faf0
M-NASCAR <mazen.nassar@education.nsw.gov.au>
2026-08-17 14:01:57 +1000

    Updated Main code
    Created a function to simplify the code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 from machine import ADC, Pin
-import utime
-soil = ADC(Pin(26)) # Connect Soil moisture sensor data to Raspberry pi pico GP26 
+import time
 import neopixel
 
 
@@ -13,20 +12,22 @@ soil = ADC(Pin(26)) # Setting up the sensor by connecting to to GPIO pin 26 (ADC
 #Calibraton values
 min_moisture= 200 # ADC reading in air (Dry)
 max_moisture=35000 #ADC reading in water (wet)    
-readDelay = 0.5 # time between readings   
+readDelay = 0.5 # time between readings
+moisture_percent = 0
 
- 
+def getPercentage():
+    value = soil.read_u16()
+    moisture = (value - min_moisture) / (max_moisture - min_moisture) #Caclulates the percentage by finding
+    if value - min_moisture != 0:
+            moisture_percent = moisture * 100
+        else:
+            moisture_percent = 0      # Needed to prevent zerodivisible error
+
  
 
  
 while True: # prints the reading
-    
-    if value - min_moisture != 0:
-         moisture_percent = moisture * 100
-    else:
-        moisture_percent = 0      # Needed to prevent zerodivisible error
-
-
+    getPercentage()
 
     if moisture_percent > 70:
         colour = (0, 0, 255) #sets colour to blue
@@ -38,7 +39,7 @@ while True: # prints the reading
         colour = (255,0,0)
 
     pixels.fill(colour)
-    pixels.write
+    pixels.write()
 
     print(f"Moisture: {moisture_percent}%" + "  Value: " + str(value)) # Prints moisture levels (Temporary)
     time.sleep(readDelay) # set a delay between readings
@@ -46,4 +47,7 @@ while True: # prints the reading
 
 
 
+
     
+colour = (0,0,0)
+pixel.fill()
